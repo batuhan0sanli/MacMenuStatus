9268f799867e1e28136fcbbfbb9a040745986428
Batuhan Şanlı <batuhansanli@gmail.com>
2023-02-22 07:31:56 +0300

    add check_dns widgets

---
+++ src/widgets/check_dns.py
@@ -0,0 +1,30 @@
+import os
+from src.base.status import Status
+from src.base.widget_builder import WidgetBuilder
+
+
+class CheckDNS(WidgetBuilder):
+    name = "Check DNS"
+    settings = {'network': 'Wi-Fi'}
+    settings_schema = {
+        "network": str,
+        "dns": str,
+    }
+
+    def _check(self) -> Status:
+        cmd_response = os.popen("networksetup -getdnsservers {}".format(self.settings.get('network'))).read().strip()
+        if self.settings.get('dns') in cmd_response:
+            return Status.success()
+        else:
+            return Status.error('DNS is not set to {}'.format(self.settings.get('dns')))
+
+
+if __name__ == "__main__":
+    settings = {
+        "network": "Wi-Fi",
+        "dns": "192.168.1.100",
+    }
+
+    obj = CheckDNS(settings=settings)
+    obj.update()
+    print(obj.status.message)
