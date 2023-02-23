82c8aaa1c335d9731fe6c8783f5fe96cda39b34c
Batuhan Şanlı <batuhansanli@gmail.com>
2023-02-23 23:42:22 +0300

    add version info

---
+++ src/__version__.py
@@ -0,0 +1,8 @@
+__title__ = "MacMenuStatus"
+__description__ = "MacMenuStatus is a status bar app for macOS that shows the status of your services."
+__url__ = "https://github.com/batuhan0sanli/MacMenuStatus"
+__version__ = "0.0.1"
+__author__ = "Batuhan Şanlı"
+__author_email__ = "batuhansanli@gmail.com"
+__license__ = "GPLv3"
+__copyright__ = "2023 Batuhan Şanlı"
