e82bf8a1a8f078fa6e0e5bbcfb510f054944d531
Batuhan Şanlı <batuhansanli@gmail.com>
2023-02-27 17:41:11 +0300

    add global config settings

--- src/config/config.py
+++ src/config/config.py
@@ -5,11 +5,23 @@ import yaml
 
 class Config:
     _instance = None
+    path = os.path.expanduser('~/.config/macmenustatus/')
+    file_name = 'config.yaml'
+    config_path = os.path.join(path, file_name)
+    example_config_path = os.path.join(os.path.dirname(__file__), 'example_config.yaml')
 
     def __init__(self):
         self._config = None
-        self.path = os.path.join(os.path.dirname(__file__), 'example_config.yaml')
+        self.__initialize()
         self.load()
+
+    def __initialize(self):
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+        if not os.path.exists(self.config_path):
+            with open(self.config_path, 'w') as file:
+                with open(self.example_config_path) as example:
+                    file.write(example.read())
 
     def __new__(cls):
         if not cls._instance:
@@ -20,5 +32,5 @@ class Config:
         return self._config.get(item) or default
 
     def load(self):
-        with open(self.path) as file:
+        with open(self.config_path) as file:
             self._config = yaml.load(file, Loader=yaml.FullLoader)
