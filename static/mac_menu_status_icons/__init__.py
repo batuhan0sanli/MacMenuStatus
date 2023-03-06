ec5f628590bbd75e7db8cb89a935647af49cd752
Batuhan Şanlı <batuhansanli@gmail.com>
2023-03-06 22:43:56 +0300

    add error flip flop

--- static/mac_menu_status_icons/__init__.py
+++ static/mac_menu_status_icons/__init__.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from rumps import Timer
 
 
 @dataclass(frozen=True, init=False)
@@ -10,3 +11,14 @@ class MacMenuStatusIcons:
     red_logo: str = 'static/mac_menu_status_icons/red_logo.png'
     green_logo: str = 'static/mac_menu_status_icons/green_logo.png'
     yellow_logo: str = 'static/mac_menu_status_icons/yellow_logo.png'
+
+    @classmethod
+    def _error_flip_flop_callback(cls, sender):
+        if sender.icon == cls.red_logo:
+            sender.icon = cls.black_logo
+        else:
+            sender.icon = cls.red_logo
+
+    @classmethod
+    def error_flip_flop(cls, sender) -> Timer:
+        return Timer(lambda _: cls._error_flip_flop_callback(sender), 0.5)
